8b2c2012cd9ed9c8fbfd631317341822c6b16e1e
GNamimates <gnamimates188@gmail.com>
2025-03-22 02:51:12 +0800

    added automatic loading of folders

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
--- code here
+for _, path in pairs(listFiles("primitive")) do require(path) end
+
+for _, path in pairs(listFiles("generic")) do require(path) end
+
+if host:isHost() then
+	for _, path in pairs(listFiles("host")) do require(path) end
+end
