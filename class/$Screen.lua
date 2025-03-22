287731645cc23a0e1bc8191b96bab23c2a11e2c0
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-03-23 03:39:17 +0800

    ScreenAPI separated API with object methods

--- class/$Screen.lua
+++ class/$Screen.lua
@@ -4,26 +4,29 @@ local canvas = GNUI.getScreenCanvas()
 local Box = require"library.GNUI.primitives.box"
 
 local screens = {}
-local currentPage
+local currentScreen
 
+---@class ScreenAPI
+local ScreenAPI = {}
+ScreenAPI.__index = ScreenAPI
 
 ---@class GNUI.Screen : GNUI.Box
 ---@field name string
 ---@field ON_ENTER Signal
 ---@field ON_EXIT Signal
 ---@field background boolean
-Screen = {}
+local Screen = {}
 Screen.__index = function (self, key)
-	return rawget(self, key) or Screen[key] or Box[key]
+	return rawget(self, key) or Box[key]
 end
 
 ---@param name string
 ---@param background boolean?
 ---@return GNUI.Screen
-function Screen.new(name,background)
+function ScreenAPI.new(name,background)
 	if type(background) == "nil" then background = true end
 	
-	local new = GNUI.newBox(canvas)
+	local new = GNUI.newBox(canvas):setAnchorMax()
 	new.name = name
 	new.ON_ENTER = Signal.new()
 	new.ON_EXIT = Signal.new()
@@ -37,43 +40,45 @@ function Screen.new(name,background)
 end
 
 
-function Screen.setPage(name)
-	local page = screens[name]
+function ScreenAPI.setPage(name)
+	local screen = screens[name]
 	if not screens[name] then
-		page = screens.default
+		screen = screens.default
 		warn("Page '"..name.."' does not exist. Defaulting to 'default' page.")
 	end
-	if currentPage ~= page then
-		if currentPage then
-			currentPage:setVisible(false)
-			currentPage.ON_EXIT:invoke()
+	if currentScreen ~= screen then
+		if currentScreen then
+			currentScreen:setVisible(false)
+			currentScreen.ON_EXIT:invoke()
 		end
 		
-		currentPage = page
+		currentScreen = screen
 		
-		currentPage.ON_ENTER:invoke()
-		currentPage:setVisible(true)
+		currentScreen.ON_ENTER:invoke()
+		currentScreen:setVisible(true)
 	end
 end
 
-Screen.new("default")
-Screen.setPage("default")
+ScreenAPI.new("default")
+ScreenAPI.setPage("default")
 
 
 ---Returns the page with the given name, or the default page if no name is given.
 ---@param name string?
 ---@return GNUI.Screen
-function Screen.getScreen(name)
+function ScreenAPI.getScreen(name)
 	return screens[name or "default"]
 end
 
 
 ---Returns a table containing the names of all pages.
 ---@return string[]
-function Screen.getScreens()
+function ScreenAPI.getScreens()
 	local names = {}
 	for name in pairs(screens) do
 		table.insert(names,name)
 	end
 	return names
 end
+
+_G.Screen = ScreenAPI
