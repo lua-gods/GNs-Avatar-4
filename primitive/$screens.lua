fc8cb8d27b22efd024e6e6eaeeb1c4e15ef9b18f
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-03-23 03:21:13 +0800

    added APIs to screens

--- primitive/$screens.lua
+++ primitive/$screens.lua
@@ -1,9 +1,9 @@
 ---@diagnostic disable: return-type-mismatch
 local GNUI = require"library.GNUI.main"
-local screen = GNUI.getScreenCanvas()
+local canvas = GNUI.getScreenCanvas()
 local Box = require"library.GNUI.primitives.box"
 
-local pages = {}
+local screens = {}
 local currentPage
 
 
@@ -12,35 +12,35 @@ local currentPage
 ---@field ON_ENTER Signal
 ---@field ON_EXIT Signal
 ---@field background boolean
-Page = {}
-Page.__index = function (self, key)
-	return rawget(self, key) or Page[key] or Box[key]
+Screen = {}
+Screen.__index = function (self, key)
+	return rawget(self, key) or Screen[key] or Box[key]
 end
 
 ---@param name string
 ---@param background boolean?
 ---@return GNUI.Screen
-function Page.new(name,background)
+function Screen.new(name,background)
 	if type(background) == "nil" then background = true end
 	
-	local new = GNUI.newBox(screen)
+	local new = GNUI.newBox(canvas)
 	new.name = name
 	new.ON_ENTER = Signal.new()
 	new.ON_EXIT = Signal.new()
 	new.background = background and true or false
-	if pages[name] then
+	if screens[name] then
 		error("A page with the name '"..name.."' already exists.",2)
 	end
-	pages[name] = new
-	setmetatable(new,Page)
+	screens[name] = new
+	setmetatable(new,Screen)
 	return new
 end
 
 
-function Page.setPage(name)
-	local page = pages[name]
-	if not pages[name] then
-		page = pages.default
+function Screen.setPage(name)
+	local page = screens[name]
+	if not screens[name] then
+		page = screens.default
 		warn("Page '"..name.."' does not exist. Defaulting to 'default' page.")
 	end
 	if currentPage ~= page then
@@ -55,5 +55,25 @@ function Page.setPage(name)
 		currentPage:setVisible(true)
 	end
 end
-Page.new("default")
-Page.setPage("default")
+
+Screen.new("default")
+Screen.setPage("default")
+
+
+---Returns the page with the given name, or the default page if no name is given.
+---@param name string?
+---@return GNUI.Screen
+function Screen.getScreen(name)
+	return screens[name or "default"]
+end
+
+
+---Returns a table containing the names of all pages.
+---@return string[]
+function Screen.getScreens()
+	local names = {}
+	for name in pairs(screens) do
+		table.insert(names,name)
+	end
+	return names
+end
