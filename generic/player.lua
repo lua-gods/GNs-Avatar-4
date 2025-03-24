94cd00ebe26bce5fa26ff1bede4c397d268e3825
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-03-24 15:15:51 +0800

    added player

---
+++ generic/player.lua
@@ -0,0 +1,10 @@
+
+vanilla_model.PLAYER:setVisible(false)
+models.player.Base.Torso.Body.Cape:setPrimaryTexture("CAPE")
+
+vanilla_model.CAPE:setVisible(false)
+vanilla_model.ELYTRA:setVisible(false)
+models.player.Base.Torso.Body.LeftElytra:scale(1.1,1.1,2.4)
+models.player.Base.Torso.Body.RightElytra:scale(1.1,1.1,2.4)
+
+--animations.player.california:play()
