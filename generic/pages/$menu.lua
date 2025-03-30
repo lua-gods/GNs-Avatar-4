9c9c3a3ecb5871a07cbb82b37f359e76211c4211
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-03-30 23:07:44 +0800

    cinematic bar

--- generic/pages/$menu.lua
+++ generic/pages/$menu.lua
@@ -1,11 +1,22 @@
+
 local GNUI = require"library.GNUI.main"
 local Theme = require"library.GNUI.theme"
+
+local quickTween = require"library.quickTween"
+
 Screen.new({
 	name = "menu",
-	background=false
+	background=true
 },function (events,screen)
 	local ribbon = GNUI.newBox(screen)
-	:setAnchor(0,0,1,1)
-	:setDimensions(0,0,10,0)
+	:setAnchor(0,0,1,0)
+	:setDimensions(0,0,0,20)
 	Theme.style(ribbon,"Background")
+	quickTween.up(ribbon,20)
+	
+	local actionBar = GNUI.newBox(screen)
+	:setAnchor(0,1,1,1)
+	:setDimensions(0,-20,0,0)
+	Theme.style(actionBar,"Background")
+	quickTween.down(actionBar,20)
 end)
