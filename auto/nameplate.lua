d57125847ee8e78c6ddc8ca10623236d3c9fbdcd
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-06-07 05:20:20 +0800

    removed useless nameplate code

--- auto/nameplate.lua
+++ auto/nameplate.lua
@@ -1,19 +1,19 @@
 local name = "GNanimates"
 
-local hover = {{text="Hello"}}
-
 local from = vectors.hexToRGB("#d3fc7e")
 local to = vectors.hexToRGB("#33984b")
 
+-- {"text":"test","clickEvent":{"action":"suggest_command","value":"test"}}}
+
 local output = {}
--- {"hoverEvent":{"action":"show_text","contents":{}}}
+
 local lname = #name
 for i = 1, lname, 1 do
 	local w = i/lname
 	output[i] = {
 		color="#"..vectors.rgbToHex(math.lerp(from,to,w)),
 		text=name:sub(i,i),
-		hoverEvent={action="show_text",contents=hover}
+		--hoverEvent={action="show_text",contents=hover},
 	}
 end
 
