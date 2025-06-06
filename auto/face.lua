7461c0a1a8dc7d3056e810152a10c6850c802472
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-06-07 05:20:35 +0800

    I am now expressive

---
+++ auto/face.lua
@@ -0,0 +1,15 @@
+models.player.Base.Torso.Head.Face.Sclera
+:setPrimaryRenderType("EMISSIVE_SOLID")
+:setColor(0.7,0.7,0.7)
+
+
+animations.player.Blink:speed(9)
+local blinkTimer = 0
+
+events.TICK:register(function ()
+	blinkTimer = blinkTimer - 1
+	if blinkTimer <= 0 then
+		blinkTimer = math.random(0.5,5) * 20
+		animations.player.Blink:stop():play()
+	end
+end)
