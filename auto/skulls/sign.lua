6b9838f0c23be6d46c7cbb390b45eeceaec8f3c5
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-06-10 04:35:31 +0800

    its a sign

---
+++ auto/skulls/sign.lua
@@ -0,0 +1,56 @@
+local Skull = require("lib.skull")
+local Color = require("lib.color")
+
+
+function hash(str)
+	local hash = 0
+	for i = 1, #str do
+		local c = str:byte(i)
+		hash = (hash * math.pi + c) % 100000 -- keep it within 5 digits
+	end
+	return hash
+end
+
+
+local SCALE = 0.845
+
+models.fez.hat:scale(SCALE,SCALE,SCALE)
+
+local function sterializeText(text)
+	return text:gsub("\n",""):gsub('"',''):gsub("'",""):gsub("\\","")
+end
+
+---@type SkullIdentity|{}
+local identity = {
+	name = "sign",
+	
+	processHud = {
+		ON_ENTER = function (skull, model)
+			model:newItem("icon"):item("minecraft:oak_sign"):pos(0,4,0)
+		end
+	},
+	
+	processHat = {
+		ON_ENTER = function (skull, model)
+			local sign = model:newBlock("sign"):pos(-8,6,-8)
+			local ok,err = pcall(sign.block,sign,"minecraft:"..skull.params[1].."_sign")
+			if not ok then
+				sign:block("oak_sign")
+			end
+			for i = 1, 4, 1 do
+				model:newText("line"..i):text('{"text":"'..sterializeText(skull.params[i+1] or "")..'","color":"black"}'):alignment("CENTER")
+				:pos(0,22.7-(i-1)*1.7,-0.68)
+				:scale(0.16)
+			end
+			
+			for i = 1, 4, 1 do
+				model:newText("line"..(i+4)):text('{"text":"'..sterializeText(skull.params[i+5] or "")..'","color":"black"}'):alignment("CENTER")
+				:pos(0,22.7-(i-1)*1.7,0.68)
+				:rot(0,180,0)
+				:scale(0.16)
+			end
+		end
+	}
+}
+
+Skull.registerIdentity(identity)
