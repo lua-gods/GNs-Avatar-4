d02199418d2b92b77036a67744803fe591d58ec9
GN68 <81744903+GN68@users.noreply.github.com>
2025-08-29 08:11:01 +0800

    gonoi

--- auto/interface.lua
+++ auto/interface.lua
@@ -1,21 +1,18 @@
 #host
 
-if not host:isHost() or true then return end
+if not host:isHost() then return end
 
 local GNUI = require("lib.GNUI.main")
 --GNUI.debugMode()
 local screen = GNUI.getScreen()
-local Box = GNUI.box
 
 local Button = require("lib.GNUI.widget.button")
 local TextField = require("lib.GNUI.widget.textField")
 
-GNUI.debugMode()
-
---local btn = Button.new(screen)
---:setPos(16,16)
---:setSize(100,20)
---:setText("Hello World")
+local btn = Button.new(screen)
+:setPos(16,16)
+:setSize(100,20)
+:setText("Hello World")
 
 
 local txf = TextField.new(screen)
