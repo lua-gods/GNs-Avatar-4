703b75fb39fff299d7fba591ef55da4bd341693b
GN68 <81744903+GN68@users.noreply.github.com>
2025-10-10 22:13:30 +0800

    cleaned up nbs to use internal APIs

--- auto/apps/nbs.lua
+++ auto/apps/nbs.lua
@@ -5,38 +5,22 @@ local zlib = require("lib.zlib")
 local Skull = require("lib.skull")
 
 
-local function give(item)
-	if player:isLoaded() then
-		local id = player:getNbt().SelectedItemSlot
-		sounds:playSound("minecraft:entity.item.pickup",client:getCameraPos():add(client:getCameraDir()),1,1)
-		host:setSlot("hotbar."..id,item)
-	end
-end
-
-function head(nbt,name)
-	name = name or "GN's Head"
-	give(Skull.makeSkull(nbt,name))
-end
-
-
 ---@type GNUI.App
 return {
 	name = "NBS Head",
 	icon = "minecraft:note_block",
 	start = function ()
-		FileDialog.new().ITEM_CONFIRMED:register(function (path,name)
-			local buffer = data:createBuffer()
-			buffer:readFromStream(file:openReadStream(path))
-			buffer:setPosition(0)
-			
-			local data = buffer:readBase64(buffer:available())
-			local item = Skull.makeSkull({nbs={data}},name)
-			buffer:close()
-			
-			if #item < 65536*999 then
-				give(item)
-				print("Generated ("..#item..")")
-			end
-		end)
+FileDialog.new().ITEM_CONFIRMED:register(function (path,name)
+	local buffer = data:createBuffer()
+	buffer:readFromStream(file:openReadStream(path))
+	buffer:setPosition(0)
+	
+	local data = buffer:readBase64(buffer:available())
+	local item = Skull.makeSkull({nbs={data}},name)
+	buffer:close()
+	
+	Skull.makeSkull({ogg=item},name)
+	print("Generated ("..#item..")")
+end)
 	end
 }
